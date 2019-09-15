e0ace823e8c32713df6d2aa9547a2fc05491b824
LizbethVera <lizveraduran@gmail.com>
2019-09-14 20:37:06 -0400

    Creando los datos a tulizar

--- src/main/java/Domain/Agenda.java
+++ src/main/java/Domain/Agenda.java
@@ -1,4 +1,17 @@
 package Domain;
 
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Agenda {
+    private @Id @GeneratedValue Long id;
+    private String fecha;
+    private String hora;
+    private String tratamiento;
+    private String Odontologo;
+    
 }
