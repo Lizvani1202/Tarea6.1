577f335dd03244622fdb1ac4f54c57e2e38d5a80
LizbethVera <lizveraduran@gmail.com>
2019-09-14 20:38:31 -0400

    Creando los getters y setters

--- src/main/java/Domain/Agenda.java
+++ src/main/java/Domain/Agenda.java
@@ -13,5 +13,54 @@ public class Agenda {
     private String hora;
     private String tratamiento;
     private String Odontologo;
-    
+
+    Agenda(){
+    }
+
+    public Agenda(String fecha, String hora, String tratamiento, String odontologo) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.tratamiento = tratamiento;
+        Odontologo = odontologo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public String getOdontologo() {
+        return Odontologo;
+    }
+
+    public void setOdontologo(String odontologo) {
+        Odontologo = odontologo;
+    }
 }
