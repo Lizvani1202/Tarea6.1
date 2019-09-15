d5371bfa4247a4d2f4ec8a6a52e2654e2212394a
LizbethVera <lizveraduran@gmail.com>
2019-09-14 21:20:10 -0400

    Poniendo la parte de get, post, put, y delete que corresponde a http

--- src/main/java/Configuracion/AgendaControlador.java
+++ src/main/java/Configuracion/AgendaControlador.java
@@ -1,4 +1,45 @@
 package Configuracion;
 
+
+import Domain.Agenda;
+import Repository.AgendaRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class AgendaControlador {
+    private final AgendaRepository agendaRepository;
+
+    public AgendaControlador(AgendaRepository agendaRepository) {
+        this.agendaRepository = agendaRepository;
+    }
+
+    @GetMapping("/agenda")
+    Agenda newAgenda(@RequestBody Agenda newAgenda){
+        return agendaRepository.save(newAgenda);
+    }
+
+    @GetMapping("/agenda/{id}")
+    Agenda one(@PathVariable Long id){
+        return agendaRepository.findById(id).orElseThrow(()-> AgendaNotFoundException(id));
+
+    }
+    @PutMapping("/agenda/{id}")
+    Agenda reemplazandoAgenda(@RequestBody Agenda newAgenda, @PathVariable Long id){
+        return agendaRepository.findById(id).map(agenda -> {
+            agenda.setFecha(newAgenda.getFecha());
+            agenda.setHora(newAgenda.getHora());
+            agenda.setTratamiento(newAgenda.getTratamiento());
+            agenda.setOdontologo(newAgenda.getOdontologo());
+            return agendaRepository.save(agenda);
+        })
+                .orElseGet(()->{
+                newAgenda.setId(id);
+                return agendaRepository.save(newAgenda);
+        });
+    }
+
+    @DeleteMapping("/agenda/{id}")
+    public void elimanarAgenda(@PathVariable Long id){
+        agendaRepository.deleteById(id);
+    }
 }
